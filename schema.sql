20293949a79fa6ecbba24c935d12d0db0845ee69
eronni1 <150188402+eronni1@users.noreply.github.com>
2024-04-23 19:18:59 -0400

    replace name w username

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE rate_my_course;
 -- Creating Users table
 CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
@@ -22,7 +22,7 @@ CREATE TABLE Reviews (
     course_id VARCHAR(50) NOT NULL,
     user_id INT NOT NULL,
     rating INT,
-    final_grade CHAR(2),
+    final_grade CHAR(1),
     comment TEXT,
     timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (course_id) REFERENCES Courses(course_id),
