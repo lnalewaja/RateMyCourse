7515e3a5f9a6124cc9d4fce914b68d49f9b6f509
Kendall_Tart <kenbazonct14@gmail.com>
2024-04-28 18:42:40 -0400

    Added is_oauth to users db

--- schema.sql
+++ schema.sql
@@ -5,7 +5,8 @@ CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    is_oauth bool default FALSE
 );
 
 -- Creating Courses table
